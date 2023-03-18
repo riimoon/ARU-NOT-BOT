96f3070b991f67b622a44d896dac39dd07770d9b
riimoon <mishay50@gmail.com>
2023-03-18 20:03:25 +0000

    interactive.sh

---
+++ interactive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Hello there! My name is Aru"
+read -p Hello || read -p Hi
+read -p "What is your name?" name
+echo "Nice to meet you $name"
+
+echo "I am not a bot, I am Aru"
+read -p "What do you do cutepie" q1
+
+echo "Nice to meet you cute"
+read -p "HEY, what do you do?"
+echo "WoooW, this is Soo Cool" $q1
+
+echo "If you have any questions just let me know pls :)"
+
