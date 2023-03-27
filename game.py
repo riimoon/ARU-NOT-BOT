f5f586be1886d3a0864588e22e2d26ef8ed50b98
riimoon <mishay50@gmail.com>
2023-03-27 20:49:46 +0000

    game.py

---
+++ game.py
@@ -0,0 +1,203 @@
+#-------------------------------------------Game start---------------------------------------------
+import os
+import time
+from pyfiglet import Figlet
+
+
+
+def display(msg,style):
+    print('\n'*10)
+    f = Figlet(style)
+    print(f.renderText(msg))
+    
+
+while True:
+    os.system('cls')
+    display('          Quiz Game', 'slant')
+    print('\n'*5)
+    print('\t\t     Press s to start the game.')
+
+    ch = input('')
+    
+
+    if ch == 's':
+        break
+        os.system('cls')
+
+        ######################Game
+        print('\n'*2)
+        print('     X ------------------------Quiz Game--------------------------- X')
+        print('\n'*2)
+
+
+##
+
+question1 = {
+    "question": "What is the core of Linux Operating System?",
+    "choices": ["A. Shell", "B. Kernel", "C. Command", "D. Terminal"],
+    "answer": "B"
+}
+
+# Print the question and answer choices
+print(question1["question"])
+for choice in question1["choices"]:
+    print(choice)
+
+# Ask the user to select an answer
+user_answer = input("Enter your answer (A, B, C, D): ")
+
+# Check if the answer is correct
+if user_answer == question1["answer"]:
+    print("Correct👍")
+else:
+    print("Sorry, that's incorrect😰")
+
+##
+question2 = {
+    "question": "What Linus Torvalds Created?",
+    "choices": ["A. Linux", "B. Debian", "C. Fedora", "D. Slackware"],
+    "answer2": "A"
+}
+
+# Print the question and answer choices
+print(question2["question"])
+for choice in question2["choices"]:
+    print(choice)
+
+# Ask the user to select an answer
+user_answer = input("Enter your answer (A, B, C, D): ")
+
+# Check if the answer is correct
+if user_answer == question2["answer2"]:
+    print("Correct👍")
+else:
+    print("Sorry, that's incorrect😰")
+
+###
+question3 = {
+    "question": "Linux is which kind of Operating System?",
+    "choices": ["A. Multi Tasking", "B. Multi Process", "C. Multi User", "D. All of the above"],
+    "answer3": "C"
+}
+
+# Print the question and answer choices
+print(question3["question"])
+for choice in question3["choices"]:
+    print(choice)
+
+# Ask the user to select an answer
+user_answer = input("Enter your answer (A, B, C, D): ")
+
+# Check if the answer is correct
+if user_answer == question3["answer3"]:
+    print("Correct👍")
+else:
+    print("Sorry, that's incorrect😰")
+
+####
+question4 = {
+    "question": "Which option of rm command is used to remove a directory including all its subdirectories?",
+    "choices": ["A. –b", "B. –o", "C.  –p", "D. –r"],
+    "answer4": "D"
+}
+
+# Print the question and answer choices
+print(question4["question"])
+for choice in question4["choices"]:
+    print(choice)
+
+# Ask the user to select an answer
+user_answer = input("Enter your answer (A, B, C, D): ")
+
+# Check if the answer is correct
+if user_answer == question4["answer4"]:
+    print("Correct👍")
+else:
+    print("Sorry, that's incorrect😰")
+
+#####
+question5 = {
+    "question": "What is the command to count the number of characters in a file?",
+    "choices": ["A. grep", "B. wc", "C.  count", "D.  cut"],
+    "answer5": "B"
+}
+
+# Print the question and answer choices
+print(question5["question"])
+for choice in question5["choices"]:
+    print(choice)
+
+# Ask the user to select an answer
+user_answer = input("Enter your answer (A, B, C, D): ")
+
+# Check if the answer is correct
+if user_answer == question5["answer5"]:
+    print("Correct👍")
+else:
+    print("Sorry, that's incorrect😰")
+
+######
+
+question6 = {
+    "question": "Which of the following commands displays user id in its output?",
+    "choices": ["A. ls –l", "B. help", "C.  date", "D.  ls"],
+    "answer6": "A"
+}
+
+# Print the question and answer choices
+print(question6["question"])
+for choice in question6["choices"]:
+    print(choice)
+
+# Ask the user to select an answer
+user_answer = input("Enter your answer (A, B, C, D): ")
+
+# Check if the answer is correct
+if user_answer == question6["answer6"]:
+    print("Correct👍")
+else:
+    print("Sorry, that's incorrect😰")
+
+#######
+question7 = {
+    "question": "What commands may a user on a Linux system run to quickly gather basic information about themselves.",
+    "choices": ["A. id", "B. whoami", "C.  users", "D.  who"],
+    "answer7": "B"
+}
+
+# Print the question and answer choices
+print(question7["question"])
+for choice in question7["choices"]:
+    print(choice)
+
+# Ask the user to select an answer
+user_answer = input("Enter your answer (A, B, C, D): ")
+
+# Check if the answer is correct
+if user_answer == question7["answer7"]:
+    print("Correct👍")
+else:
+    print("Sorry, that's incorrect😰")
+
+########
+question8 = {
+    "question": "Choose the best answer - Which of the following symbolic representations matches the numeric permissions of 0640?",
+    "choices": ["A. rw-r-----", "B. r-xr-----", "C.  ---r--r-x", "D.  rwxr-x---"],
+    "answer8": "A"
+}
+
+# Print the question and answer choices
+print(question8["question"])
+for choice in question8["choices"]:
+    print(choice)
+
+# Ask the user to select an answer
+user_answer = input("Enter your answer (A, B, C, D): ")
+
+# Check if the answer is correct
+if user_answer == question8["answer8"]:
+    print("Correct👍")
+else:
+    print("Sorry, that's incorrect😰")
+
+
